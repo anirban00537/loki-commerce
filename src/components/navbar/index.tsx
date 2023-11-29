6a9d431cbb37c8369d4f624307b7d98c66fbd8f6
anirban_itech <anirban00537@gmail.com>
2023-11-29 18:49:16 +0600

    updated

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,20 +1,11 @@
 'use client';
-import { Navbar } from 'flowbite-react';
+import { Dropdown, Navbar } from 'flowbite-react';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { RiSearchLine, RiShoppingCart2Line } from 'react-icons/ri';
 
 import { cn } from '@/lib/utils';
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from '@/components/ui/navigation-menu';
 import CartSheet from '@/components/sheet/cart.sheet';
 function NavbarComp() {
   const [cartCount, setCartCount] = useState(0);
@@ -83,112 +74,39 @@ function NavbarComp() {
             </div>
             <Navbar.Toggle />
           </div>
+          <Navbar.Collapse>
+            {/* Set the text color for Navbar links */}
+            <Navbar.Link href='#' active className='text-blue-700'>
+              Home
+            </Navbar.Link>
+            <Navbar.Link href='#' className='text-gray-800'>
+              Shop
+            </Navbar.Link>
+            <Navbar.Link href='#' className='text-gray-800'>
+              Categories
+            </Navbar.Link>
+            <Navbar.Link href='#' className='text-gray-800'>
+              Cart
+              {cartCount > 0 && (
+                <span className='ml-1 text-red-500'>({cartCount})</span>
+              )}
+            </Navbar.Link>
+            <Navbar.Link href='#' className='text-gray-800'>
+              Account
+            </Navbar.Link>
 
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <Link href='/docs' legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Fashion
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Trending</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <ul className='grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
-                    <li className='row-span-3'>
-                      <NavigationMenuLink asChild>
-                        <a
-                          className='from-muted/50 to-muted flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none focus:shadow-md'
-                          href='/'
-                        >
-                          <div className='mb-2 mt-4 text-lg font-medium'>
-                            shadcn/ui
-                          </div>
-                          <p className='text-muted-foreground text-sm leading-tight'>
-                            Beautifully designed components built with Radix UI
-                            and Tailwind CSS.
-                          </p>
-                        </a>
-                      </NavigationMenuLink>
-                    </li>
-                    <ListItem href='/docs' title='Introduction'>
-                      Re-usable components built using Radix UI and Tailwind
-                      CSS.
-                    </ListItem>
-                    <ListItem href='/docs/installation' title='Installation'>
-                      How to install dependencies and structure your app.
-                    </ListItem>
-                    <ListItem
-                      href='/docs/primitives/typography'
-                      title='Typography'
-                    >
-                      Styles for headings, paragraphs, lists...etc
-                    </ListItem>
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Hot Deals</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <ul className='grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] '>
-                    {components.map((component) => (
-                      <ListItem
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
-                      >
-                        {component.description}
-                      </ListItem>
-                    ))}
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href='/docs' legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    About Us
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
-                <Link href='/docs' legacyBehavior passHref>
-                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Blogs
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
+            {/* Set the text color for Dropdown items */}
+            <Dropdown label='Templates' inline>
+              <Dropdown.Item className='text-gray-800'>Dashboard</Dropdown.Item>
+              <Dropdown.Item className='text-gray-800'>Settings</Dropdown.Item>
+              <Dropdown.Item className='text-gray-800'>Earnings</Dropdown.Item>
+              <Dropdown.Item className='text-gray-800'>Sign out</Dropdown.Item>
+            </Dropdown>
+          </Navbar.Collapse>
         </Navbar>
       </div>
     </div>
   );
 }
-const ListItem = React.forwardRef<
-  React.ElementRef<'a'>,
-  React.ComponentPropsWithoutRef<'a'>
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            'hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors',
-            className
-          )}
-          {...props}
-        >
-          <div className='text-sm font-medium leading-none'>{title}</div>
-          <p className='text-muted-foreground line-clamp-2 text-sm leading-snug'>
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = 'ListItem';
+
 export default NavbarComp;
