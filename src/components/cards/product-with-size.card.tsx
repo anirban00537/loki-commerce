634e6c830e268326e0fd1241d89663d9028fac1c
anirban00537 <anirban00537@gmail.com>
2023-12-10 16:45:19 +0600

    updated ecommerce

--- src/components/cards/product-with-size.card.tsx
+++ src/components/cards/product-with-size.card.tsx
@@ -81,7 +81,7 @@ export function ProductCardWithSize({
         </div>
         <CardContent className='mt-3 grid gap-4'>
           <CardHeader className='p-0'>
-            <CardTitle className={cn('truncate text-gray-600', className)}>
+            <CardTitle className={cn('mb-3 truncate text-gray-600', className)}>
               {name}
             </CardTitle>
             <div className='flex items-center '>
@@ -121,7 +121,7 @@ export function ProductCardWithSize({
           </CardHeader>
         </CardContent>
         <CardFooter>
-          <Button className='w-full'>
+          <Button className='w-full bg-white text-black border'>
             <ShoppingCart className='mr-2' size={20} />
             Add to Cart
           </Button>
--- src/section/common/quick-products.tsx
+++ src/section/common/quick-products.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BigCard from '@/components/cards/big-card';
+import Link from 'next/link';
 
 interface Product {
   url: string;
@@ -43,6 +44,17 @@ const QuickProducts = () => {
 
   return (
     <section className='container mx-auto p-10 px-0 md:p-8 md:px-0 md:py-12'>
+      <div className='mb-8 flex items-center justify-between '>
+        <h2 className='text-3xl font-bold uppercase text-slate-800'>
+          Premium products
+        </h2>
+        <Link
+          href='/collections'
+          className='flex items-center justify-center rounded-md border px-2 py-1 text-sm text-gray-600 hover:bg-gray-200 hover:text-gray-700'
+        >
+          Show All
+        </Link>
+      </div>
       <section className='grid grid-cols-1 items-start gap-10 p-5 sm:grid-cols-1 md:grid-cols-2 md:p-0 xl:grid-cols-3 2xl:grid-cols-4 '>
         {products.map((product, index) => (
           <BigCard key={index} {...product} />
