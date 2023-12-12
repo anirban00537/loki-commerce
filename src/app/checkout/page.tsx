b7738c387ab3ee0a64d439f085eff09d19cbdf4a
anirban00537 <anirban00537@gmail.com>
2023-12-13 00:44:33 +0600

    updated

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,32 +1,58 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { X } from 'lucide-react';
-const CartItem = ({ product }: any) => (
-  <li className='flex items-center justify-between space-x-5 border-b border-gray-200 py-4'>
-    <div className='flex items-center space-x-4'>
-      <div className='h-20 w-20 flex-shrink-0 overflow-hidden rounded-lg border border-gray-200 bg-white'>
-        <img
-          className='h-full w-full object-cover'
-          src={product.imageSrc}
-          alt={product.name}
-        />
+const CartItem = ({ product }: any) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const increaseQuantity = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const decreaseQuantity = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  return (
+    <li className='flex items-center justify-between space-x-5 border-b border-gray-300 py-4'>
+      <div className='flex items-start space-x-4'>
+        <div className='h-28 w-28 flex-shrink-0 overflow-hidden rounded-lg'>
+          <img
+            className='h-full w-full object-cover'
+            src={product.imageSrc}
+            alt={product.name}
+          />
+        </div>
+        <div className=''>
+          <p className='text-base font-bold text-gray-900'>{product.name}</p>
+          <p className='text-sm text-gray-500'>{product.color}</p>
+          <p className='text-xl font-bold text-gray-500'>{product.price}</p>
+        </div>
       </div>
-      <div>
-        <p className='text-base font-bold text-gray-900'>{product.name}</p>
-        <p className='text-sm text-gray-500'>{product.color}</p>
+      <div className='flex items-center'>
+        <div className='flex items-center'>
+          <button
+            className='rounded-lg border px-4 py-2 text-sm'
+            onClick={decreaseQuantity}
+          >
+            -
+          </button>
+          <div className='mx-5'>{quantity}</div>
+          <button
+            className=' rounded-lg border px-4 py-2 text-sm'
+            onClick={increaseQuantity}
+          >
+            +
+          </button>
+        </div>
+        <button className='ml-4 rounded-lg border  px-4 py-2 text-xs'>
+          Remove
+        </button>
       </div>
-    </div>
-    <div className='flex items-center space-x-4'>
-      <p className='text-base font-bold text-gray-900'>{product.price}</p>
-      <button
-        type='button'
-        className='text-gray-400 transition-all duration-200 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-900 focus:ring-offset-2'
-      >
-        <span className='sr-only'>Remove</span>
-        <X className='h-5 w-5' />
-      </button>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 const products = [
   {
@@ -68,10 +94,10 @@ const products = [
 ];
 const Checkout = () => {
   return (
-    <div className='container my-4 max-w-6xl md:my-6 md:mt-20 '>
+    <div className='container my-4  md:my-6 md:mt-20 '>
       <div className='mb-10 border-b md:flex md:flex-row md:items-start md:justify-between'>
-        <div className='mb-4'>
-          <h1 className='text-4xl font-semibold'>Your Card</h1>
+        <div className='mb-16'>
+          <h1 className='text-4xl font-semibold'>Your Cart</h1>
         </div>
         <div className='mt-6 flex items-center  pt-2 md:mt-0 md:space-x-4  md:pt-0'></div>
       </div>
@@ -86,240 +112,35 @@ const Checkout = () => {
               </ul>
             </div>
             <hr className='mt-6 border-gray-200' />
-            <form action='#' className='mt-6'>
-              <div className='sm:flex sm:space-x-2.5 md:flex-col md:space-x-0 lg:flex-row lg:space-x-2.5'>
-                <div className='flex-grow'>
-                  <input
-                    className='flex w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50'
-                    type='text'
-                    placeholder='Enter coupon code'
-                  />
-                </div>
-                <div className='mt-4 sm:mt-0 md:mt-4 lg:mt-0'>
-                  <button
-                    type='button'
-                    className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
-                  >
-                    Apply Coupon
-                  </button>
-                </div>
-              </div>
-            </form>
-            <ul className='mt-6 space-y-3'>
-              <li className='flex items-center justify-between text-gray-600'>
-                <p className='text-sm font-medium'>Sub total</p>
-                <p className='text-sm font-medium'>$399</p>
-              </li>
-              <li className='flex items-center justify-between text-gray-900'>
-                <p className='text-sm font-medium '>Total</p>
-                <p className='text-sm font-bold '>$114</p>
-              </li>
-            </ul>
           </div>
-          <div className='px-5 py-6 text-gray-900 md:px-8'>
+          <div className='container px-5 py-6 text-gray-900 md:px-8'>
             <div className='flow-root'>
               <div className='-my-6 divide-y divide-gray-200'>
-                <div className='py-6'>
-                  <form>
-                    <div className='mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0'>
-                      <div>
-                        <h3
-                          id='contact-info-heading'
-                          className='text-lg font-semibold text-gray-900'
-                        >
-                          Contact information
-                        </h3>
+                <div className='  '>
+                  <p className='text-base font-bold text-gray-900'>Total</p>
 
-                        <div className='mt-4 w-full'>
-                          <label
-                            className='text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-                            htmlFor='name'
-                          >
-                            Full Name
-                          </label>
-                          <input
-                            className='flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50'
-                            type='text'
-                            placeholder='Enter your name'
-                            id='name'
-                          ></input>
-                        </div>
-                      </div>
-                      <hr className='my-8' />
-                      <div className='mt-10'>
-                        <h3 className='text-lg font-semibold text-gray-900'>
-                          Payment details
-                        </h3>
-
-                        <div className='mt-6 grid grid-cols-3 gap-x-4 gap-y-6 sm:grid-cols-4'>
-                          <div className='col-span-3 sm:col-span-4'>
-                            <label
-                              htmlFor='cardNum'
-                              className='block text-sm font-medium text-gray-700'
-                            >
-                              Card number
-                            </label>
-                            <div className='mt-1'>
-                              <input
-                                className='flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50'
-                                type='text'
-                                placeholder='4242 4242 4242 4242'
-                                id='cardNum'
-                              ></input>
-                            </div>
-                          </div>
-                          <div className='col-span-2 sm:col-span-3'>
-                            <label
-                              htmlFor='expiration-date'
-                              className='block text-sm font-medium text-gray-700'
-                            >
-                              Expiration date (MM/YY)
-                            </label>
-                            <div className='mt-1'>
-                              <input
-                                type='date'
-                                name='expiration-date'
-                                id='expiration-date'
-                                autoComplete='cc-exp'
-                                className='block h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50'
-                              />
-                            </div>
-                          </div>
-
-                          <div>
-                            <label
-                              htmlFor='cvc'
-                              className='block text-sm font-medium text-gray-700'
-                            >
-                              CVC
-                            </label>
-                            <div className='mt-1'>
-                              <input
-                                type='text'
-                                name='cvc'
-                                id='cvc'
-                                autoComplete='csc'
-                                className='flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50'
-                              />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <hr className='my-8' />
-                      <div className='mt-10'>
-                        <h3 className='text-lg font-semibold text-gray-900'>
-                          Shipping address
-                        </h3>
-
-                        <div className='mt-6 grid grid-cols-1 gap-x-4 gap-y-6 sm:grid-cols-3'>
-                          <div className='sm:col-span-3'>
-                            <label
-                              htmlFor='address'
-                              className='block text-sm font-medium text-gray-700'
-                            >
-                              Address
-                            </label>
-                            <div className='mt-1'>
-                              <input
-                                type='text'
-                                id='address'
-                                name='address'
-                                autoComplete='street-address'
-                                className='flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50'
-                              />
-                            </div>
-                          </div>
-
-                          <div>
-                            <label
-                              htmlFor='city'
-                              className='block text-sm font-medium text-gray-700'
-                            >
-                              City
-                            </label>
-                            <div className='mt-1'>
-                              <input
-                                type='text'
-                                id='city'
-                                name='city'
-                                autoComplete='address-level2'
-                                className='flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50'
-                              />
-                            </div>
-                          </div>
-
-                          <div>
-                            <label
-                              htmlFor='region'
-                              className='block text-sm font-medium text-gray-700'
-                            >
-                              State / Province
-                            </label>
-                            <div className='mt-1'>
-                              <input
-                                type='text'
-                                id='region'
-                                name='region'
-                                autoComplete='address-level1'
-                                className='flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50'
-                              />
-                            </div>
-                          </div>
-
-                          <div>
-                            <label
-                              htmlFor='postal-code'
-                              className='block text-sm font-medium text-gray-700'
-                            >
-                              Postal code
-                            </label>
-                            <div className='mt-1'>
-                              <input
-                                type='text'
-                                id='postal-code'
-                                name='postal-code'
-                                autoComplete='postal-code'
-                                className='flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50'
-                              />
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <hr className='my-8' />
-                      <div className='mt-10'>
-                        <h3 className='text-lg font-semibold text-gray-900'>
-                          Billing information
-                        </h3>
-
-                        <div className='mt-6 flex items-center'>
-                          <input
-                            id='same-as-shipping'
-                            name='same-as-shipping'
-                            type='checkbox'
-                            defaultChecked
-                            className='h-4 w-4 rounded border-gray-300 text-black focus:ring-black'
-                          />
-                          <div className='ml-2'>
-                            <label
-                              htmlFor='same-as-shipping'
-                              className='text-sm font-medium text-gray-900'
-                            >
-                              Same as shipping information
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-
-                      <div className='mt-10 flex justify-end border-t border-gray-200 pt-6'>
-                        <button
-                          type='button'
-                          className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
-                        >
-                          Make payment
-                        </button>
-                      </div>
-                    </div>
-                  </form>
+                  <ul className='mt-6 space-y-3'>
+                    <li className='flex items-center justify-between border-b py-5 text-gray-400'>
+                      <p className='text-sm font-medium'>Sub total</p>
+                      <p className='text-sm font-bold text-gray-800  '>$399</p>
+                    </li>
+                    <li className='flex items-center justify-between border-b py-5 text-gray-400 '>
+                      <p className='text-sm font-medium '>Tax</p>
+                      <p className='text-sm font-bold text-gray-800'>$114</p>
+                    </li>
+                    <li className='flex items-center justify-between  py-5 text-gray-400 '>
+                      <p className='text-sm font-bold text-gray-950  '>
+                        Order Total
+                      </p>
+                      <p className='text-sm font-bold text-gray-950'>$514</p>
+                    </li>
+                    <button
+                      type='button'
+                      className='w-full rounded-2xl  bg-black p-4 text-xl font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
+                    >
+                      Checkout
+                    </button>
+                  </ul>
                 </div>
               </div>
             </div>
